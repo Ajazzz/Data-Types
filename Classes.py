68101d18caced0206e94fc8b373cb33296f9403e
Ajaz <41190679+Ajazzz@users.noreply.github.com>
2023-04-02 22:26:17 +0530

    Update and rename Classes to Classes.py

---
+++ Classes.py
@@ -0,0 +1,59 @@
+
+# In Python, classes are a way of defining user-defined data types. 
+# A class is like a blueprint for creating objects, which are instances of the class.
+
+# Classes in Python encapsulate data and functions that operate on that data, providing a way to organize and reuse code. 
+# They can be used to model real-world objects, such as a car or a person, or to represent abstract concepts, such as a queue or a stack.
+
+Here is an example of a simple class in Python:
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def say_hello(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+        
+        
+#In this example, the Person class has a constructor method __init__, 
+#which takes two arguments name and age and initializes the corresponding instance variables. 
+#It also has a method say_hello that prints a message using the instance variables.
+
+#To create an object of the Person class, we can do:
+
+person = Person("Alice", 30)
+
+
+# This creates a new instance of the Person class with the name "Alice" and age 30. 
+We can call the say_hello method on this instance to print a greeting:
+
+person.say_hello()  # Output: Hello, my name is Alice and I am 30 years old.
+
+#This is just a basic example of a class in Python. 
+Classes can have many more attributes and methods, and can be used to build complex programs.
+
+
+
+
+######
+#In Python, classes are used to create new user-defined data types that encapsulate both data and the functions that operate on that data. 
+#They are a fundamental concept in object-oriented programming (OOP) and are used to create objects that have certain attributes (data) and,
+#methods (functions) that can be called to operate on that data.
+
+#Here are some common use cases for classes in Python:
+
+#Modeling real-world objects: Classes can be used to represent real-world objects like people, cars, 
+#or buildings, by defining their attributes and behaviors as class variables and methods.
+
+#Code organization: Classes can be used to organize related functions and data into a single entity. 
+#This makes the code more modular, easier to understand, and easier to maintain.
+
+#Inheritance: Classes can be used to implement inheritance, where one class can inherit properties and methods from another class. 
+#This allows for code reuse and helps to avoid redundancy.
+
+#Polymorphism: Classes can be used to implement polymorphism, where different objects can have the same interface but behave differently. 
+#This allows for more flexible and modular code.
+
+
+#Overall, classes are a powerful feature in Python that enable developers to create complex and organized code, and build more advanced programs.
